9bebd54422b7931b9c9757613794623a2115c352
l2h <842817794@qq.com>
2016-06-04 09:19:26 +0800

    没有修改

---
+++ src/main/java/com/l2h/eam/sys/mapper/DepartmentMapper.java
@@ -0,0 +1,40 @@
+package com.l2h.eam.sys.mapper;
+
+import com.l2h.eam.utils.domain.TreeNode;
+
+import java.util.List;
+
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┃    神兽保佑,代码无bug
+ * 　　　　┃　　　┃
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * 项目名称 ：L2H_EAM
+ * 包名     ：com.l2h.eam.sys.mapper
+ * 创建人   ：l2h
+ * 创建时间 ：2016-06-01
+ * 功能说明 ：
+ */
+public interface DepartmentMapper {
+    /**
+     * 加载本部门及下属所有部门
+     * @param dpet_code
+     * @return 返回所有部门节点，每个部门对应一个树节点
+     */
+   List<TreeNode> getDpetsByLike(String dpet_code);
+
+}
