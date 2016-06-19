2d904367a178cb28bbe51783bbbea22b6f2ed71c
l2h <842817794@qq.com>
2016-06-19 17:11:25 +0800

    修改请求路径。统一为：{模块}/{控制器}/{方法}

--- src/main/java/com/l2h/eam/sys/controller/UserController.java
+++ src/main/java/com/l2h/eam/sys/controller/UserController.java
@@ -5,21 +5,43 @@ import com.l2h.eam.sys.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Created by Administrator on 2016-05-29.
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┃    神兽保佑,代码无bug
+ * 　　　　┃　　　┃
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * 项目名称 ：L2H_EAM
+ * 包名     ：com.l2h.eam.sys.controller
+ * 创建人   ：l2h
+ * 创建时间 ：2016-06-05
+ * 功能说明 ：用户管理controller
  */
 @Controller
+@SessionAttributes("userInfo")
 public class UserController {
 
     @Autowired
     private UserService userService;
-    public void setUserService(UserService userService) {
-        this.userService = userService;
-    }
     @RequestMapping(method = RequestMethod.GET,path = "/getUser")
     public String getUserByAccout(ModelMap modelMap,String account)
     {
@@ -28,7 +50,20 @@ public class UserController {
         user.setPassword("123456");
         user.setUser_name("l2h");
         userService.insertUser(user);
-
         return "hello";
+    }
+
+    /**
+     * 函数说明：返回用户管理页面
+     * @param user session中保存的当前用户信息
+     * @return
+     */
+    @RequestMapping(value = "sys/user/userMsgPage.do")
+    public  String getUserManagerPage(@ModelAttribute("userInfo") User user)
+    {
+         User user1 = user;
+        String name = user1.getUser_name();
+         return "sys/sys_usermanager";
+
     }
 }
